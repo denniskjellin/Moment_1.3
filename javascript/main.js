40a9fd4285963a010a4f6badd6f3b73841f8f6d9
Dennis Kjellin <denniskjellin@hotmail.com>
2022-04-05 14:02:16 +0200

    created about us page, css styling

--- javascript/main.js
+++ javascript/main.js
@@ -7,3 +7,19 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
+
+// variabels
+let currentLocation = location.href;
+let menuItem = document.querySelectorAll('nav a');
+let menuLength = menuItem.length;
+
+//looping trough
+for (let i = 0; i < menuLength; i++) {
+    //giving the navbar-links + active class on the current navbar href tag
+    if (menuItem[i].href === currentLocation) {
+        menuItem[i].className = "active";
+    }
+}
+
+
+window.onload = init;
